064acdffee00b4ef04fef3879f501ded9966927e
Sabrina Jahan <onusabrina1998@gmail.com>
2023-07-18 23:43:04 +0600

    final

--- orientation_app/lib/main.dart
+++ orientation_app/lib/main.dart
@@ -1,125 +1,95 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Orientation app',
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Orientation _orientation = Orientation.landscape;
+  List<String> images = [
+    'https://cdn.pixabay.com/photo/2016/11/21/12/29/owl-1845060_1280.jpg',
+    'https://cdn.pixabay.com/photo/2014/02/27/16/08/owl-275942_640.jpg',
+    'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_640.jpg',
+    'https://cdn.pixabay.com/photo/2016/01/08/11/57/butterflies-1127666_640.jpg',
+    'https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529_640.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        _orientation = orientation;
+        return Scaffold(
+          appBar: AppBar(title: Text('Orientation app')),
+          body: _buildBody(),
+        );
+      },
     );
+  }
+
+  Widget _buildBody() {
+    if (_orientation == Orientation.portrait) {
+      return Column(
+        children: [
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: NetworkImage('https://picsum.photos/id/237/200/300'),
+          ),
+          Expanded(
+            child: Text('This is a portrait layout',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          for (String image in images)
+            Image.network(
+              image,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+
+        ],
+      );
+    } else {
+      return Row(
+        children: [
+          CircleAvatar(
+              radius: 100,
+              backgroundImage:
+              NetworkImage('https://picsum.photos/id/237/200/300',
+              )
+          ),
+          Text('This is the landscape layout',
+            style: TextStyle(fontWeight: FontWeight.bold),),
+
+          for (String image in images)
+            Image.network(
+              image,
+              height: 100,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+
+        ],
+      );
+    }
   }
 }
